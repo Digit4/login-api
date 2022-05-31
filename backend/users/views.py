d9faf4392a5bc6fec31a4b8b0ca2dc9dd95c7f9f
Dhaval Pantojee <dpantojee775@gmail.com>
2022-05-31 18:13:25 +0530

    Added code for implementation of Token expiry

--- backend/users/views.py
+++ backend/users/views.py
@@ -23,7 +23,7 @@ class LoginView(APIView):
 		user = User.objects.filter(email=email).first()
 
 		if user is None:
-			raise AuthenticationFailed("User Not Found!")
+			return AuthenticationFailed("User Not Found!")
 
 		if not user.check_password(password):
 			raise AuthenticationFailed("Incorrect Password")
@@ -33,10 +33,44 @@ class LoginView(APIView):
 		'exp': datetime.datetime.utcnow()+datetime.timedelta(minutes=60),
 		'iat': datetime.datetime.utcnow()
 		}
+		time_format = "%d/%m/%y %H:%M:%S.%f"
+		timeout = str(payload['exp'])
 
 		token = jwt.encode(payload, 'secret', algorithm='HS256')
 
-		response = Response({"jwt_token":token})
+		response = Response({"jwt_token":token,"expiration":timeout})
 		response.set_cookie(key='jwt_token', value=token,httponly=True)
+		response.set_cookie(key="expiration", value=timeout,httponly=True)
+
+		return response
+
+class UserView(APIView):
+
+	def get(self,request):
+		token = request.COOKIES.get('jwt_token')
+		exp = request.COOKIES.get('expiration')
+
+		if not token:
+			return Response({"error":"Unauthenticated User"}, status=401)
+
+		try:
+			payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+
+		except (jwt.ExpiredSignatureError):
+			return Response({"error":"Authentication Expired"}, status=403)
+
+		user = User.objects.filter(id=payload['id']).first()
+		serializer = UserSerializer(user)
+		return Response({"user_data":serializer.data,"expiration": exp})
+
+
+class LogoutView(APIView):
+	def post(self,request):
+		response = Response()
+		response.delete_cookie('jwt_token')
+		response.delete_cookie('expiration')
+		response.data = {
+			"logout": "successful"
+		}
 
 		return response
